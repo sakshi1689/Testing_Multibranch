f2aba6575d0dfc7420904a7f2d0ea7e918dced1e
sakshi1689 <sakshi_saman@yahoo.co.in>
2018-03-06 12:51:19 -0500

    Update Jenkinsfile.groovy

--- test/Jenkinsfile.groovy
+++ test/Jenkinsfile.groovy
@@ -1,22 +1,13 @@
-Jenkinsfile (Declarative Pipeline)
-pipeline {
-    agent any
-
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Building..'
-            }
+ def jsonParameter = '''{
+ "scm": {
+            "execute": true,
+            "integrationPoints": [{
+                "method": "scm",
+                "scm": {
+                    "scmType": "GIT",
+                    "scmUrl": "https://github.com/sakshi1689/Testing_Multibranch",
+                    "scmBranch": "#CurrentGitBranchName#"
+                }
+            }]
         }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-    }
-}
+		}'''
